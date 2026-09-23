c24e17fc888dfe248438b7419c2cdc89f455eb9d
plattform26 <plattform26@gmail.com>
2026-09-23 11:22:39 -0600

    chore: remove test scripts before prod merge

--- scripts/list-users.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-const prisma = new PrismaClient()
-async function main() {
-  const users = await prisma.user.findMany({ select: { email: true, id: true } })
-  console.log(JSON.stringify(users, null, 2))
-}
-main().finally(() => prisma.$disconnect())
